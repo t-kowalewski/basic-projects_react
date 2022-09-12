84bac30527b9898cfc89c827d57f1fd6b48de3b9
Tony Kowalewski <tony@kowalewski.dev>
2022-09-12 23:17:11 +0200

    Lorem - my implem

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,43 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
-  
+  const [paragraphs, setParagraphs] = useState([]);
+  const [amount, setAmount] = useState('0');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setParagraphs(data.slice(0, Number(amount)));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>lorem ipsum generator</h3>
+
+      <form className='lorem-form'>
+        <label htmlFor='paragraphSelector'>Paragraphs</label>
+        <input
+          type='number'
+          min='0'
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
+
+        <button type='submit' className='btn' onClick={submitHandler}>
+          Generate
+        </button>
+      </form>
+
+      <article className='lorem-article'>
+        {paragraphs.map((paragraph) => {
+          return <p key={new Date().getTime().toString()}>{paragraph}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
