4a18f914dcb5cfabb6834cd637235998c30613fc
Tony Kowalewski <tony@kowalewski.dev>
2022-09-15 21:10:06 +0200

    Color Generator - init comm

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,51 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [hexColor, setHexColor] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [colorsList, setColorsList] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log('Submited!');
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+
+        <form onSubmit={submitHandler}>
+          <input
+            type='text'
+            name='color'
+            id='color'
+            placeholder='#71a507'
+            value={hexColor}
+            onChange={(e) => {
+              setHexColor(e.target.value);
+            }}
+          />
+
+          <button type='submit' className='btn'>
+            Submit
+          </button>
+        </form>
+
+        <section>
+          <article></article>
+        </section>
+      </section>
+
+      <section className='colors'>
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
