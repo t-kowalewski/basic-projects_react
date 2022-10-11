8d4520fad144d5d267e709a4ddfbfad2cb3b57cc
Tony Kowalewski <tony@kowalewski.dev>
2022-10-11 18:52:37 +0200

    Navbar - init comm

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,59 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
 
-export default Navbar
+        <div className='links-container show-container'>
+          <ul className='links'>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>About</a>
+            </li>
+            <li>
+              <a href='#'>Projects</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
+            <li>
+              <a href='#'>Profile</a>
+            </li>
+          </ul>
+        </div>
+
+        <ul className='social-icons'>
+          <li>
+            <a href='https://twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
