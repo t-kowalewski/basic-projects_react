cf7b0b4f3a23b002bc6ed2b0aa7c1ee92925d535
Tony Kowalewski <tony@kowalewski.dev>
2022-11-21 23:24:12 +0100

    Submenu - add general structure

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,32 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppConetex = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+  // const openSidebar = () => {
+  //   setIsSidebarOpen(true);
+  // };
+  const toggleSidebar = () => {
+    setIsSidebarOpen((currState) => !currState);
+  };
+  const toggleSubmenu = () => {
+    setIsSubmenuOpen((currState) => !currState);
+  };
+
+  return (
+    <AppConetex.Provider
+      value={{ isSidebarOpen, isSubmenuOpen, toggleSidebar, toggleSubmenu }}
+    >
+      {children}
+    </AppConetex.Provider>
+  );
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppConetex);
+};
