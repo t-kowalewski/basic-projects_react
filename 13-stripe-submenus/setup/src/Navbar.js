cc6184ad414b2ae0506a0674ff1e88697a195ae5
Tony Kowalewski <tony@kowalewski.dev>
2022-11-22 23:21:51 +0100

    Submenu - add navbar structure

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+import data from './data';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { toggleSubmenu, toggleSidebar } = useGlobalContext();
 
-export default Navbar
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='stripe' className='nav-logo' />
+          <button className='btn toggle-btn' onClick={toggleSidebar}>
+            <FaBars />
+          </button>
+        </div>
+
+        <ul className='nav-links'>
+          {data.map((item, index) => {
+            // console.log(index);
+            return (
+              <li key={index}>
+                <button className='link-btn'>{item.page}</button>
+              </li>
+            );
+          })}
+        </ul>
+
+        <button className='btn signin-btn'>Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
