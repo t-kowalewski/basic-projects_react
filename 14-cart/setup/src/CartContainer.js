d7251379f4324b8a5804eebf666cd57655d1c308
Tony Kowalewski <tony@kowalewski.dev>
2022-12-05 21:47:22 +0100

    Cart - init comm

--- 14-cart/setup/src/CartContainer.js
+++ 14-cart/setup/src/CartContainer.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import CartItem from './CartItem'
-import { useGlobalContext } from './context'
+import React from 'react';
+import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart } = useGlobalContext();
+
   if (cart.length === 0) {
     return (
       <section className='cart'>
@@ -13,20 +14,23 @@ const CartContainer = () => {
           <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
-    )
+    );
   }
+
   return (
     <section className='cart'>
       {/* cart header */}
       <header>
         <h2>your bag</h2>
       </header>
+
       {/* cart items */}
       <div>
         {cart.map((item) => {
-          return <CartItem key={item.id} {...item} />
+          return <CartItem key={item.id} {...item} />;
         })}
       </div>
+
       {/* cart footer */}
       <footer>
         <hr />
@@ -35,6 +39,7 @@ const CartContainer = () => {
             total <span>$0.00</span>
           </h4>
         </div>
+
         <button
           className='btn clear-btn'
           onClick={() => console.log('clear cart')}
@@ -43,7 +48,7 @@ const CartContainer = () => {
         </button>
       </footer>
     </section>
-  )
-}
+  );
+};
 
-export default CartContainer
+export default CartContainer;
--- 14-cart/setup/src/CartItem.js
+++ 14-cart/setup/src/CartItem.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
+
 const CartItem = ({ id, img, title, price, amount }) => {
   return (
     <article className='cart-item'>
@@ -7,6 +8,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
+
         {/* remove button */}
         <button
           className='remove-btn'
@@ -15,6 +17,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
           remove
         </button>
       </div>
+
       <div>
         {/* increase amount */}
         <button className='amount-btn' onClick={() => console.log('increase')}>
@@ -22,8 +25,10 @@ const CartItem = ({ id, img, title, price, amount }) => {
             <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
           </svg>
         </button>
+
         {/* amount */}
         <p className='amount'>{amount}</p>
+
         {/* decrease amount */}
         <button className='amount-btn' onClick={() => console.log('decrease')}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
@@ -32,7 +37,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
         </button>
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
