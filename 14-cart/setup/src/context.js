88d3d7425b14006b69bac510c15777808fedfcd5
Tony Kowalewski <tony@kowalewski.dev>
2022-12-09 23:40:05 +0100

    Cart - add increase

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -25,10 +25,15 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'REMOVE_ITEM', payload: id });
   };
 
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
+        increase,
         removeItem,
         clearCart,
       }}
--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -2,10 +2,34 @@ const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return { ...state, cart: [] };
   }
+
   if (action.type === 'REMOVE_ITEM') {
     const newCart = state.cart.filter((item) => item.id !== action.payload);
     return { ...state, cart: newCart };
   }
+
+  if (action.type === 'INCREASE') {
+    // const selectedProduct = state.cart.find(
+    //   (prod) => prod.id === action.payload
+    // );
+    // console.log(selectedProduct);
+
+    return {
+      ...state,
+      cart: state.cart.map((item) => {
+        return item.id === action.payload
+          ? { ...item, amount: item.amount + 1 }
+          : item;
+
+        // if (item.id === action.payload) {
+        //   return { ...item, amount: item.amount + 1 };
+        // } else {
+        //   return item;
+        // }
+      }),
+    };
+  }
+
   return state;
 };
 
